1c96bc106ed594bac4377d5d5fb2daa9a473ae51
dabenav <81636248+dabenav@users.noreply.github.com>
2021-09-22 15:37:46 -0500

    Update tshoot.ps1

--- tshoot.ps1
+++ tshoot.ps1
@@ -18,7 +18,7 @@ $IP = $IPDetails.IPv4Address.IPAddress
 $Geteway = $IPDetails.IPv4DefaultGateway.NextHop
 $DNSs = (Get-DnsClientServerAddress).ServerAddresses|where{$_.length -lt '16'}
 $PublicDNS = "8.8.8.8", "1.1.1.1"
-$PublicSites = "Ow.ly", "aa.com"
+$PublicSites = "etsy.com", "aa.com"
 $domain = (Get-WmiObject win32_computersystem).Domain
 
 
