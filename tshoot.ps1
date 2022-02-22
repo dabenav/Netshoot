a97d05af7e8513501327b97963b8deddb2987a65
dabenav <81636248+dabenav@users.noreply.github.com>
2022-02-22 17:34:16 -0500

    Update tshoot.ps1

--- tshoot.ps1
+++ tshoot.ps1
@@ -58,7 +58,7 @@ $PublicIPAddress =  $(Resolve-DnsName -Name myip.opendns.com -Server 208.67.222.
 
 $PublicDNS = "8.8.8.8", "1.1.1.1"
 $PublicSites = "cisco.com", "ibm.com"
-$pingCount = 2
+$pingCount = 10
 
 
 #########################################################################################
