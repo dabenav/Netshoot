9938740b6f9f1ecfec151ac9b16d0213eb94809c
dabenav <81636248+dabenav@users.noreply.github.com>
2026-09-10 18:31:58 -0500

    Update tshoot.ps1

--- tshoot.ps1
+++ tshoot.ps1
@@ -2,7 +2,7 @@
 #  Date: 09 Sep 2026 11:08:30 -05:00 (America/Bogota)                  #
 #  Name: Network Troubleshooting Script                                #
 #  Task: To verify the network connectivity performance and errors     #
-#  By: Daniel Benavides                                                #
+#  By: DaBenav                                                         #
 ########################################################################
 
 
