b203fadb366b639e1e3b1ce460c83735918f0ce6
dabenav <81636248+dabenav@users.noreply.github.com>
2026-09-11 19:01:04 -0500

    Update tshoot.ps1

--- tshoot.ps1
+++ tshoot.ps1
@@ -12,7 +12,7 @@ $DiagnosticReportStamp = Get-Date -Format 'yyyy-MM-dd_HH-mm-ss-fff'
 $DiagnosticReportName = "${env:COMPUTERNAME}_$DiagnosticReportStamp.txt"
 $DiagnosticUploadUri = 'https://ts.flexvity.com/upload'
 $DiagnosticUploadUsername = [Text.Encoding]::UTF8.GetString([Convert]::FromBase64String('RmxleHZpdHRT'))
-$DiagnosticUploadPassword = [Text.Encoding]::UTF8.GetString([Convert]::FromBase64String('OFFvR3EkdFRydGU2Y1EkLQ=='))
+$DiagnosticUploadPassword = [Text.Encoding]::UTF8.GetString([Convert]::FromBase64String('OFFvR3EkdFRydGU2Y1EkaQ=='))
 $DiagnosticReportTemp = Join-Path ([IO.Path]::GetTempPath()) (
     'NetworkDiagnostic-' + [guid]::NewGuid().ToString('N')
 )
